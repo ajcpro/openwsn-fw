0fc7f2f48977d7e3a6cabc3230f382f69051ebcd
Antonio Cepero <iescpro@gmail.com>
2016-06-04 21:50:34 +0200

    Reuse message on echo reply
    Instead of creating a new message for the reply, modify the same received
    one to limit the amount of used memory.

--- openstack/03b-IPv6/icmpv6echo.c
+++ openstack/03b-IPv6/icmpv6echo.c
@@ -100,24 +100,27 @@ void icmpv6echo_receive(OpenQueueEntry_t* msg) {
                                (errorparameter_t)msg->length,
                                (errorparameter_t)0);
          // get a new openqueuEntry_t for the echo reply
-         reply = openqueue_getFreePacketBuffer(COMPONENT_ICMPv6ECHO);
+/*         reply = openqueue_getFreePacketBuffer(COMPONENT_ICMPv6ECHO);
          if (reply==NULL) {
             openserial_printError(COMPONENT_ICMPv6ECHO,ERR_NO_FREE_PACKET_BUFFER,
                                   (errorparameter_t)1,
                                   (errorparameter_t)0);
             openqueue_freePacketBuffer(msg);
             return;
-         }
+         }*/
+
+	 // reuse msg for reply: do not duplicate memory if a big one
+	 reply = msg;
          // take ownership over reply
          reply->creator = COMPONENT_ICMPv6ECHO;
          reply->owner   = COMPONENT_ICMPv6ECHO;
          // copy payload from msg to (end of) reply
-         packetfunctions_reserveHeaderSize(reply,msg->length);
-         memcpy(reply->payload,msg->payload,msg->length);
+/*         packetfunctions_reserveHeaderSize(reply,msg->length);
+         memcpy(reply->payload,msg->payload,msg->length);*/
          // copy source of msg in destination of reply
          memcpy(&(reply->l3_destinationAdd),&(msg->l3_sourceAdd),sizeof(open_addr_t));
          // free up msg
-         openqueue_freePacketBuffer(msg);
+         //openqueue_freePacketBuffer(msg);
          msg = NULL;
          // administrative information for reply
          reply->l4_protocol                   = IANA_ICMPv6;
